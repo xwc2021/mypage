db161a2ffa75e2997b28791169f769c6bf349ee0
xwc2023 <xiangwei71@gmail.com>
2023-03-09 13:26:42 +0800

    測試看看組合的寫法

--- EMaji/emaji.js
+++ EMaji/emaji.js
@@ -111,7 +111,38 @@ let cars_36 = cards.splice(0, 36);
 //  9    8    7
 // 12    5    4
 // 15    2    1
-pick_from(cars_36, 13);
+// pick_from(cars_36, 13);
 
+
+// C all.length取pick_count
+// 雖然會有重覆但不管
+function C(all, pick_count) {
+    // init 
+    let temp_list = [];
+    for (let i = 0; i < pick_count; ++i)
+        temp_list.push(-1);
+
+    list_all(all, 0, all.length - 1, 0, pick_count, temp_list)
+}
+
+let counter = 0;
+function list_all(all, left_borlder, right_border, now_index, pick_count, temp_list) {
+
+    for (let i = left_borlder; i <= right_border; ++i) {
+        let pick_one = all[i];
+        temp_list[now_index] = pick_one;
+
+        if (now_index == pick_count - 1)
+            console.log(++counter, temp_list);
+        else
+            list_all(all, i + 1, right_border, now_index + 1, pick_count, temp_list);
+    }
+}
+
+
+// 取5個就算超久了
+// https://gadget.chienwen.net/x/math/percomb
+// C(cars_36, 5);
+C(cars_36, 2);
 
 // 判定聽幾個洞
